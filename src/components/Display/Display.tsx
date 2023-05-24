22c3087c9cc38d62cd774ddeab4ea2cff2fcec32
Vitalij Kazuro <vitalijkazuro939@gmail.com>
2023-05-25 00:00:28 +0300

    start

---
+++ src/components/Display/Display.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './display.scss';
+
+export const Display = () => {
+  return (
+    <section className='display'>
+      <div className='display__monitor'>
+        <span className='display__monitor-count'>
+          0
+        </span>
+      </div>
+    </section>
+  )
+}
+
